7ec880ed4efd7751133a8138f1d18df8c0a87bb6
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2025-08-04 15:14:03 +0200

    chore: update build process for netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { FaInstagram, FaTiktok } from 'react-icons/fa6';
 
 function App() {
-
   return (
-    <h1>itsmz</h1>
-  )
+    <div className={'flex flex-col gap-4'}>
+      <h1>ITSMZ</h1>
+      <a href={'https://www.tiktok.com/@itsmz_dani'}>
+        <FaTiktok />
+        TikTok
+      </a>
+      <a href={'https://www.instagram.com/itsmz_dani/#'}>
+        <FaInstagram />
+        Instagram
+      </a>
+      <h2>Latest Content</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
